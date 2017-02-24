346b3bc56e146eb9d73827a7696fa8c82f08bab6
Alan Guedes <alanlivio@gmail.com>
2017-02-24 17:44:22 -0300

    refactory functions names.

--- bash-helper-functions.sh
+++ bash-helper-functions.sh
@@ -3,10 +3,10 @@
 ###############################################################################
 # Bash helper functions (github.com/alanlivio/bash_helper_functions)
 # update by: wget raw.githubusercontent.com/alanlivio/bash_helper_functions/master/bash_helper_functions.sh
-function aux_print() { echo -e "$1" | fold -w100 -s | sed '2~1s/^/  /'; }
-function log_error() { aux_print "\033[00;31m---> $1 fail\033[00m"; }
-function log_msg()   { aux_print "\033[00;33m---> $1\033[00m"; }
-function log_done()  { aux_print "\033[00;32m---> $1 done\033[00m"; }
-function log_ok()    { aux_print "\033[00;32m---> OK\033[00m"; }
-function TRY()       { $*; if test $? -ne 0; then log_error "$1" && exit 1; fi;}
+function aux-print() { echo -e "$1" | fold -w100 -s | sed '2~1s/^/  /'; }
+function log-error() { aux-print "\033[00;31m---> $1 fail\033[00m"; }
+function log-msg()   { aux-print "\033[00;33m---> $1\033[00m"; }
+function log-done()  { aux-print "\033[00;32m---> $1 done\033[00m"; }
+function log-ok()    { aux-print "\033[00;32m---> OK\033[00m"; }
+function TRY()       { "$@"; if test $? -ne 0; then log-error "$1" && exit 1; fi;}
 ###############################################################################
