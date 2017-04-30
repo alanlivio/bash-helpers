3cc92adb1ad49f3034a702e603dcc48945325ffc
Alan Guedes <alanlivio@gmail.com>
2017-04-30 18:32:39 -0300

    fix arguments usage.

--- bash-helper-functions.sh
+++ bash-helper-functions.sh
@@ -10,8 +10,8 @@
 ###############################################################################
 
 function aux-print() { echo -e "$1" | fold -w100 -s | sed '2~1s/^/  /'; }
-function log-error() { aux-print "\033[00;31m---> $@ \033[00m"; }
-function log-msg()   { aux-print "\033[00;33m---> $@\033[00m"; }
+function log-error() { aux-print "\033[00;31m---> $* \033[00m"; }
+function log-msg()   { aux-print "\033[00;33m---> $* \033[00m"; }
 function log-done()  { aux-print "\033[00;32m---> done\033[00m"; }
 function log-ok()    { aux-print "\033[00;32m---> ok033[00m"; }
 function TRY()       { "$@"; if test $? -ne 0; then log-error "$1" && exit 1; fi;}
