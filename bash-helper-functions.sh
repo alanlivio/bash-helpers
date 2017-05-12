24f419189f06ae7b5b3a08fcec581faf98135639
Alan Guedes <alanlivio@gmail.com>
2017-05-12 20:48:38 -0300

    code format with shfmt.

--- bash-helper-functions.sh
+++ bash-helper-functions.sh
@@ -11,15 +11,18 @@
 
 function aux-print() { echo -e "$1" | fold -w100 -s | sed '2~1s/^/  /'; }
 function log-error() { aux-print "\033[00;31m---> $* \033[00m"; }
-function log-msg()   { aux-print "\033[00;33m---> $* \033[00m"; }
-function log-done()  { aux-print "\033[00;32m---> done\033[00m"; }
-function log-ok()    { aux-print "\033[00;32m---> ok033[00m"; }
-function TRY()       { "$@"; if test $? -ne 0; then log-error "$1" && exit 1; fi;}
+function log-msg() { aux-print "\033[00;33m---> $* \033[00m"; }
+function log-done() { aux-print "\033[00;32m---> done\033[00m"; }
+function log-ok() { aux-print "\033[00;32m---> ok033[00m"; }
+function TRY() {
+  "$@"
+  if test $? -ne 0; then log-error "$1" && exit 1; fi
+}
 
 case "$(uname -s)" in
-   Darwin)IS_MAC=1;;
-   Linux)IS_LINUX=1;;
-   CYGWIN*|MINGW32*|MSYS*) IS_WINDOWS=1
+  Darwin) IS_MAC=1 ;;
+  Linux) IS_LINUX=1 ;;
+  CYGWIN* | MINGW32* | MSYS*) IS_WINDOWS=1 ;;
 esac
 
 ###############################################################################
@@ -32,9 +35,9 @@ function mybash-audio-create-empty() {
   gst-launch-1.0 audiotestsrc wave=4 ! audioconvert ! lamemp3enc ! id3v2mux ! filesink location="$1"
 }
 
-function mybash-audio-compress() {  
+function mybash-audio-compress() {
   : ${1?an argument is required}
-  lame -b 32  "$1".mp3 compressed"$1".mp3
+  lame -b 32 "$1".mp3 compressed"$1".mp3
 }
 
 ###############################################################################
@@ -51,16 +54,16 @@ function mybash-video-cut() {
   : ${2? an second argument is required}
   : ${3? an third argument is required}
   # ffmpeg -i video-cuted.mp4 -vcodec copy -acodec copy -ss 00:16:03 -t 00:09:34 -f mp4 "video.mp4"
-  ffmpeg -i $1  -vcodec copy -acodec copy -ss $2  -t $3  -f mp4 cuted-$1
+  ffmpeg -i $1 -vcodec copy -acodec copy -ss $2 -t $3 -f mp4 cuted-$1
 }
 
 function mybash-video-gst-side-by-side-test() {
-  gst-launch-1.0 compositor name=comp  sink_1::xpos=640 !   videoconvert ! ximagesink   videotestsrc pattern=snow  !   "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)30/1" !   timeoverlay ! queue2 ! comp.   videotestsrc pattern=smpte !   "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)10/1" !   timeoverlay ! queue2 ! comp.
+  gst-launch-1.0 compositor name=comp sink_1::xpos=640 ! videoconvert ! ximagesink videotestsrc pattern=snow ! "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)30/1" ! timeoverlay ! queue2 ! comp. videotestsrc pattern=smpte ! "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)10/1" ! timeoverlay ! queue2 ! comp.
 }
 
 function mybash-video-gst-side-by-side-args() {
   : ${2?two arguments are required}
-  gst-launch-1.0 compositor name=comp  sink_1::xpos=640 ! ximagesink  filesrc location=$1 !   "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)30/1" !   decodebin ! videoconvert ! comp. filesrc location=$2  !   "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)10/1" ! decodebin ! videoconvert ! comp.
+  gst-launch-1.0 compositor name=comp sink_1::xpos=640 ! ximagesink filesrc location=$1 ! "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)30/1" ! decodebin ! videoconvert ! comp. filesrc location=$2 ! "video/x-raw,format=AYUV,width=640,height=480,framerate=(fraction)10/1" ! decodebin ! videoconvert ! comp.
 }
 
 ###############################################################################
@@ -69,16 +72,14 @@ function mybash-video-gst-side-by-side-args() {
 
 function mybash-pygmentize-files-by-extensions-to-image() {
   : ${1?an argument is required}
-  find . -maxdepth 1 -name "*.$1" | while read -r i
-  do
+  find . -maxdepth 1 -name "*.$1" | while read -r i; do
     pygmentize -f jpeg -l xml -o $i.jpg $i
   done
 }
 
 function mybash-pygmentize-files-by-extensions-to-rtf() {
   : ${1?an argument is required}
-   find . -maxdepth 1 -name "*.$1" | while read -r i
-  do
+  find . -maxdepth 1 -name "*.$1" | while read -r i; do
     pygmentize -f jpeg -l xml -o $i.jpg $i
     pygmentize -P fontsize=16 -P fontface=consolas -l -o $i.rtf $i
   done
@@ -86,8 +87,7 @@ function mybash-pygmentize-files-by-extensions-to-rtf() {
 
 function mybash-pygmentize-files-by-extensions-to-html() {
   : ${1?an argument is required}
-   find . -maxdepth 1 -name "*.$1" | while read -r i
-  do
+  find . -maxdepth 1 -name "*.$1" | while read -r i; do
     pygmentize -O full,style=default -f html -l xml -o $i.html $i
   done
 }
@@ -127,9 +127,8 @@ function mybash-git-create-gitignore-cpp() {
   mybash-git-create-gitignore c,c++,qt,autotools,make,ninja,cmake
 }
 
-function mybash-git-find-folders-reset-clean-uninstall {
-  find -iname .git | while read -r i
-  do
+function mybash-git-find-folders-reset-clean-uninstall() {
+  find -iname .git | while read -r i; do
     cd "$(dirname $i)" || exit
     make clean
     make uninstall
@@ -140,9 +139,9 @@ function mybash-git-find-folders-reset-clean-uninstall {
 }
 
 function mybash-git-commit-formated() {
-  echo -e "\n" > /tmp/commit.txt
-  for i in $(git status -s|cut -c4-); do
-    echo -e "* $i: Likewise.">>  /tmp/commit.txt
+  echo -e "\n" >/tmp/commit.txt
+  for i in $(git status -s | cut -c4-); do
+    echo -e "* $i: Likewise." >>/tmp/commit.txt
   done
   git commit -t /tmp/commit.txt
 }
@@ -158,54 +157,54 @@ function mybash-git-list-large-files() {
 function mybash-qtcreator-project-from-git() {
   project_name="${PWD##*/}"
   touch "$project_name.config"
-  echo -e "[General]\n" > "$project_name.creator"
-  echo -e "src\n" > "$project_name.includes"
-  git ls-files > "$project_name.files"
+  echo -e "[General]\n" >"$project_name.creator"
+  echo -e "src\n" >"$project_name.includes"
+  git ls-files >"$project_name.files"
 }
 
-function mybash-atom-copy-tern-project(){
+function mybash-atom-copy-tern-project() {
   cp ~/gdrive/env/apps/.tern-project .
 }
 
 function mybash-eclipse-list-installed() {
   /opt/eclipse/eclipse \
-  -consolelog -noSplash \
-  -application org.eclipse.equinox.p2.director \
-  -listInstalledRoots
+    -consolelog -noSplash \
+    -application org.eclipse.equinox.p2.director \
+    -listInstalledRoots
 }
 
 ###############################################################################
 # android functions
 ###############################################################################
 
-function mybash-android-start-activity () {
+function mybash-android-start-activity() {
   #adb shell am start -a android.intent.action.MAIN -n com.android.browser/.BrowserActivity
   #adb shell am start -a android.intent.action.MAIN -n org.libsdl.app/org.libsdl.app.SDLActivity
   : ${1?an argument is required}
   adb shell am start -a android.intent.action.MAIN -n "$1"
 }
-function mybash-android-restart-adb (){
+function mybash-android-restart-adb() {
   sudo adb kill-server && sudo adb start-server
 }
 
-function mybash-android-get-ip(){
+function mybash-android-get-ip() {
   adb shell netcfg
   adb shell ifconfig wlan0
 }
 
-function mybash-android-enable-stdout-stderr-output(){
+function mybash-android-enable-stdout-stderr-output() {
   adb shell stop
   adb shell setprop log.redirect-stdio true
   adb shell start
 }
 
-function mybash-android-get-printscreen (){
+function mybash-android-get-printscreen() {
   adb shell /system/bin/screencap -p /sdcard/screenshot.png
   adb pull /sdcard/screenshot.png screenshot.png
   adb pull /sdcard/screenshot.png screenshot.png
 }
 
-function mybash-android-installed-package (){
+function mybash-android-installed-package() {
   : ${1?an argument is required}
   adb shell pm list packages | grep ginga
 }
@@ -225,19 +224,19 @@ function mybash-android-install-package() {
 ###############################################################################
 
 function mybash-folder-size() {
-  du -ahd 1| sort -h
+  du -ahd 1 | sort -h
 }
 
-function mybash-folder-delete-latex-files () {
+function mybash-folder-delete-latex-files() {
   find -print0 -iname "*-converted-to.pdf" -or -iname "*.aux" -or -iname "*.log" -or -iname "*.nav" -or -iname "*.out" -or -iname "*.snm" "*.synctex.gz" -or -iname "*.toc" | xargs rm
 }
 
 function mybash-folder-delete-cmake-files() {
-  rm -rf CMakeFiles/ CMakeCache.txt cmake-install.cmake  Makefile CPack* CPack* CTest* "*.cbp"
+  rm -rf CMakeFiles/ CMakeCache.txt cmake-install.cmake Makefile CPack* CPack* CTest* "*.cbp"
 }
 
 function mybash-folder-delete-binary-files() {
-  find -print0 -iname "*.a" -or -iname "*.o" -or -iname "*.so" -or -iname "*.Plo" -or -iname "*.la" -or -iname "*.log" -or -iname "*.tmp"| xargs rm
+  find -print0 -iname "*.a" -or -iname "*.o" -or -iname "*.so" -or -iname "*.Plo" -or -iname "*.la" -or -iname "*.log" -or -iname "*.tmp" | xargs rm
 }
 
 function mybash-folder-find-cpp-files() {
@@ -254,12 +253,12 @@ function mybash-folder-find-autotools-files() {
 
 function mybash-image-reconize-text() {
   : ${1?an argument is required}
-  tesseract -l eng "$1"  "$1.txt"
+  tesseract -l eng "$1" "$1.txt"
 }
 
 function mybash-imagem-compress() {
   : ${1?an argument is required}
-  pngquant "$1"   --force --quality=70-80 -o "compressed-$1"
+  pngquant "$1" --force --quality=70-80 -o "compressed-$1"
 }
 
 function mybash-imagem-compress2() {
@@ -273,21 +272,20 @@ function mybash-imagem-compress2() {
 
 function mybash-pdf-remove-password() {
   : ${1?an argument is required}
-  qpdf --decrypt "$1"  "unlocked-$1"
+  qpdf --decrypt "$1" "unlocked-$1"
 }
 
 function mybash-pdf-remove-watermark() {
   : ${1?an argument is required}
-  sed -e "s/THISISTHEWATERMARK/ /g" < "$1"  > nowatermark.pdf
+  sed -e "s/THISISTHEWATERMARK/ /g" <"$1" >nowatermark.pdf
   pdftk nowatermark.pdf output repaired.pdf
   mv repaired.pdf nowatermark.pdf
 }
 
 function mybash-pdf-compress() {
   : ${1?an argument is required}
-  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1-compressed.pdf  $1
+  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1-compressed.pdf $1
 }
-
 
 function mybash-pdf-convert-to() {
   : ${1?an argument is required}
@@ -300,7 +298,7 @@ function mybash-pdf-convert-to() {
 
 function mybash-rename-lowercase-dash() {
   : ${1?an argument is required}
-  rename 'y/A-Z/a-z/;s/_/-/g;s/\./-/g;s/ /-/g;s/---/-/g;s/-pdf/.pdf/g' "$@" &> /dev/null
+  rename 'y/A-Z/a-z/;s/_/-/g;s/\./-/g;s/ /-/g;s/---/-/g;s/-pdf/.pdf/g' "$@" &>/dev/null
 }
 
 ###############################################################################
@@ -313,7 +311,7 @@ function mybash-network-ip() {
 
 function mybash-network-arp-scan() {
   ip=$(mybash-network-ip)
-  sudo nmap --host-timeout 1s --script smb-os-discovery.nse -RsP --version-light --system-dns  $ip/24 | grep -e 'Nmap scan report' -e 'Host is' -e 'MAC Address:'| sed 's/Nmap scan/----------------------------------------\nNmap scan/'
+  sudo nmap --host-timeout 1s --script smb-os-discovery.nse -RsP --version-light --system-dns $ip/24 | grep -e 'Nmap scan report' -e 'Host is' -e 'MAC Address:' | sed 's/Nmap scan/----------------------------------------\nNmap scan/'
 }
 
 ###############################################################################
@@ -341,15 +339,15 @@ function mybash-user-reload-bashrc() {
 }
 
 function mybash-user-fix-ssh-permissions() {
-  sudo chmod  700 ~/.ssh/ &&\
-  sudo chmod  755 ~/.ssh/* &&\
-  sudo chmod  600 ~/.ssh/id_rsa &&\
-  sudo chmod  644 ~/.ssh/id_rsa.pub
+  sudo chmod 700 ~/.ssh/ \
+    && sudo chmod 755 ~/.ssh/* \
+    && sudo chmod 600 ~/.ssh/id_rsa \
+    && sudo chmod 644 ~/.ssh/id_rsa.pub
 }
 
 function mybash-user-send-ssh-keys() {
   : ${1?an argument is required}
-  ssh "$1" 'cat - >> ~/.ssh/authorized_keys' < ~/.ssh/id_rsa.pub
+  ssh "$1" 'cat - >> ~/.ssh/authorized_keys' <~/.ssh/id_rsa.pub
 }
 
 ###############################################################################
@@ -361,13 +359,13 @@ function mybash-vscode-run-as-root() {
   sudo code --user-data-dir="~/.vscode" "$1"
 }
 
-function mybash-vscode-install-packages(){
+function mybash-vscode-install-packages() {
   PKGS_TO_INSTALL=""
   INSTALLED_LIST="$(code --list-extensions)"
   for i in "$@"; do
     # echo $i
-    echo "$INSTALLED_LIST" | grep "^$i" &> /dev/null
-    if test $? != 0 ; then
+    echo "$INSTALLED_LIST" | grep "^$i" &>/dev/null
+    if test $? != 0; then
       PKGS_TO_INSTALL="$PKGS_TO_INSTALL $i"
     fi
   done
@@ -393,7 +391,7 @@ function mybash-gnome-reset-keybindings() {
 }
 
 function mybash-gnome-update-default-apps() {
-  sudo update-desktop-database /usr/share/gnome/applications  /usr/share/applications /usr/local/share/applications, /var/lib/snapd/desktop/applications
+  sudo update-desktop-database /usr/share/gnome/applications /usr/share/applications /usr/local/share/applications, /var/lib/snapd/desktop/applications
 }
 
 function mybash-gnome-background-screensaver-black() {
@@ -405,13 +403,13 @@ function mybash-gnome-background-screensaver-black() {
   gsettings set org.gnome.desktop.screensaver secondary-color "#000000"
   gsettings set org.gnome.desktop.screensaver color-shading-type "solid"
   gsettings set org.gnome.desktop.screensaver picture-uri ''
-}       
+}
 
 function mybash-gnome-show-version() {
   gnome-shell --version
-  mutter --version|head -n 1
+  mutter --version | head -n 1
   gnome-terminal --version
-  gnome-text-editor  --version
+  gnome-text-editor --version
 }
 
 function mybash-gnome-gdm-restart() {
@@ -426,13 +424,12 @@ function mybash-gnome-settings-reset() {
 function mybash-gnome-settings-save-to-file() {
   : ${1?an argument is required}
   : ${2? an second argument is required}
-  gsettings list-recursively $1| sed -e 's/@as //g' -e 's/, /,/g' > $2
+  gsettings list-recursively $1 | sed -e 's/@as //g' -e 's/, /,/g' >$2
 }
 
 function mybash-gnome-settings-load-from-file() {
   : ${1?an argument is required}
-  sed -e 's/@as //g' -e 's/, /,/g' $1 | while read -r i
-  do
+  sed -e 's/@as //g' -e 's/, /,/g' $1 | while read -r i; do
     gsettings set $i
   done
 }
@@ -441,7 +438,7 @@ function mybash-gnome-settings-diff-and-file() {
   : ${1?an argument is required}
   : ${2? an second argument is required}
   TMP_FILE=/tmp/gnome-settings-diff
-  gsettings list-recursively $1| sed -e 's/@as //g' -e 's/, /,/g' > $TMP_FILE
+  gsettings list-recursively $1 | sed -e 's/@as //g' -e 's/, /,/g' >$TMP_FILE
   diff $TMP_FILE $2
 }
 
@@ -449,8 +446,8 @@ function mybash-gnome-settings-diff-and-file() {
 # vlc functions
 ###############################################################################
 
-function mybash-vlc-youtube-playlist-extension(){
-  wget https://dl.opendesktop.org/api/files/download/id/1473753829/149909-playlist_youtube.lua  -P /tmp/
+function mybash-vlc-youtube-playlist-extension() {
+  wget https://dl.opendesktop.org/api/files/download/id/1473753829/149909-playlist_youtube.lua -P /tmp/
   sudo install /tmp/149909-playlist_youtube.lua /usr/lib/vlc/lua/playlist/
 }
 
@@ -473,20 +470,19 @@ function mybash-node-install-packages() {
   fi
   NPM_PKGS_TO_INSTALL=""
   for i in "$@"; do
-    npm list -g $i &> /dev/null
-    if test $? != 0 ; then
+    npm list -g $i &>/dev/null
+    if test $? != 0; then
       NPM_PKGS_TO_INSTALL="$NPM_PKGS_TO_INSTALL $i"
     fi
   done
   echo "NPM_PKGS_TO_INSTALL=$NPM_PKGS_TO_INSTALL"
   if test -n "$NPM_PKGS_TO_INSTALL"; then
-    if test -f pakcage.json;then cd /tmp/; fi 
-      sudo -H npm install -g $NPM_PKGS_TO_INSTALL
-      sudo -H npm update
-    if test -f pakcage.json;then cd -; fi
-  fi  
+    if test -f pakcage.json; then cd /tmp/; fi
+    sudo -H npm install -g $NPM_PKGS_TO_INSTALL
+    sudo -H npm update
+    if test -f pakcage.json; then cd -; fi
+  fi
 }
-
 
 ###############################################################################
 # python functions
@@ -497,8 +493,8 @@ function mybash-python-install-packages() {
   sudo -H pip install --upgrade pip
   PIP_PKGS_TO_INSTALL=""
   for i in "$@"; do
-    pip show $i &> /dev/null
-    if test $? != 0 ; then
+    pip show $i &>/dev/null
+    if test $? != 0; then
       PIP_PKGS_TO_INSTALL="$PIP_PKGS_TO_INSTALL $i"
     fi
   done
@@ -512,18 +508,18 @@ function mybash-python-install-packages() {
 # deb functions
 ###############################################################################
 
-function mybash-deb-upgrade(){
+function mybash-deb-upgrade() {
   log-msg "upgrade deb packages"
   sudo apt-get -y update
   sudo apt-get -y upgrade
 }
 
-function mybash-deb-install-packages(){
+function mybash-deb-install-packages() {
   log-msg "install deb packages"
   PKGS_TO_INSTALL=""
   for i in "$@"; do
-    dpkg --status "$i" &> /dev/null
-    if test $? != 0 ; then
+    dpkg --status "$i" &>/dev/null
+    if test $? != 0; then
       PKGS_TO_INSTALL="$PKGS_TO_INSTALL $i"
     fi
   done
@@ -533,7 +529,7 @@ function mybash-deb-install-packages(){
   fi
 }
 
-function mybash-deb-clean(){
+function mybash-deb-clean() {
   log-msg "apt-get clean autoclean autoremove"
   sudo apt-get -y remove --purge
   sudo apt-get -y -f install
@@ -542,13 +538,13 @@ function mybash-deb-clean(){
   sudo apt-get -y autoremove
 }
 
-function mybash-deb-remove-packages(){
+function mybash-deb-remove-packages() {
   log-msg "remove deb packages"
 
   PKGS_TO_REMOVE=""
   for i in "$@"; do
-    dpkg --status "$i" &> /dev/null
-    if test $? -eq 0 ; then
+    dpkg --status "$i" &>/dev/null
+    if test $? -eq 0; then
       PKGS_TO_REMOVE="$PKGS_TO_REMOVE $i"
     fi
   done
@@ -558,18 +554,18 @@ function mybash-deb-remove-packages(){
   fi
 }
 
-function mybash-deb-remove-orphan-packages(){
+function mybash-deb-remove-orphan-packages() {
   log-msg "remove orphan deb packages"
 
   PKGS_ORPHAN_TO_REMOVE=""
   for i in $(deborphan); do
     FOUND_EXCEPTION=false
     for j in "$@"; do
-      if test "$i" = "$j" ;then
+      if test "$i" = "$j"; then
         FOUND_EXCEPTION=true
         break
-      fi;
-    done;
+      fi
+    done
     if ! $FOUND_EXCEPTION; then
       PKGS_ORPHAN_TO_REMOVE="$PKGS_ORPHAN_TO_REMOVE $i"
     fi
@@ -580,11 +576,11 @@ function mybash-deb-remove-orphan-packages(){
   fi
 }
 
-function mybash-deb-wget-install(){
+function mybash-deb-wget-install() {
   : ${1?an argument is required}
   DEB_NAME=$(basename $1)
-  if test ! -f /tmp/$DEB_NAME; then 
-    wget $1 -P /tmp/; 
+  if test ! -f /tmp/$DEB_NAME; then
+    wget $1 -P /tmp/
   fi
   sudo dpkg -i /tmp/$DEB_NAME
 }
@@ -593,25 +589,28 @@ function mybash-deb-wget-install(){
 # wget functions
 ###############################################################################
 
-mybash-wget-extract-to(){
+mybash-wget-extract-to() {
   : ${1?an argument is required}
   : ${2? an second argument is required}
   FILE_NAME_ORIG=$(basename $1)
   FILE_NAME=$(echo $FILE_NAME_ORIG | sed -e's/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g' | xargs echo -e)
   FILE_EXTENSION=${FILE_NAME##*.}
 
-  if test ! -f /tmp/$FILE_NAME; then 
+  if test ! -f /tmp/$FILE_NAME; then
     log-msg "fetching $FILE_NAME"
     wget $1 -P /tmp/
   fi
   log-msg "extracting $FILE_NAME"
   case $FILE_EXTENSION in
     gz) # consider tar.gz
-      tar -xf /tmp/$FILE_NAME -C $2;;
+      tar -xf /tmp/$FILE_NAME -C $2
+      ;;
     bz2) # consider tar.bz2
-      tar -xjf /tmp/$FILE_NAME -C $2;;
+      tar -xjf /tmp/$FILE_NAME -C $2
+      ;;
     zip)
-      unzip /tmp/$FILE_NAME -d $2/;;
+      unzip /tmp/$FILE_NAME -d $2/
+      ;;
   esac
 }
 
@@ -619,10 +618,10 @@ mybash-wget-extract-to(){
 # list functions
 ###############################################################################
 
-mybash-list-sorted-by-size(){
-  du -h|sort -h
+mybash-list-sorted-by-size() {
+  du -h | sort -h
 }
 
-mybash-list-recursive-sorted-by-size(){
-  du -ah|sort -h
+mybash-list-recursive-sorted-by-size() {
+  du -ah | sort -h
 }
