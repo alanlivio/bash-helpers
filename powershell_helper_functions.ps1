6a78c664916bfc74e8d223234a6afbc61f185923
Alan Guedes <alanlivio@gmail.com>
2018-11-01 19:44:00 -0700

    group similiar functions

--- powershell_helper_functions.ps1
+++ powershell_helper_functions.ps1
@@ -1,16 +1,29 @@
-function hf_powershell_profile(){
+
+# ---------------------------------------
+# user functions
+# ---------------------------------------
+
+function hf_user_reload(){
+  Import-Module -Force -Global \Users\alan\gdrive\env\scripts\powershell_helper_functions.ps1
+}
+
+function hf_user_profile_init(){
   New-Item $PROFILE -Type File -Force
   echo "Import-Module -Force -Global \Users\alan\gdrive\env\scripts\powershell_helper_functions.ps1" > $PROFILE
   echo "cd ~" >> $PROFILE
 }
 
+# ---------------------------------------
+# powershell functions
+# ---------------------------------------
+
 function hf_powershell_enable_script(){
   Set-ExecutionPolicy unrestricted
 }
 
-function hf_reload(){
-  Import-Module -Force -Global \Users\alan\gdrive\env\scripts\powershell_helper_functions.ps1
-}
+# ---------------------------------------
+# clean functions
+# ---------------------------------------
 
 function hf_clean(){
     echo "hf_clean"
@@ -36,6 +49,49 @@ function hf_clean(){
     Remove-Item -Force -Recurse -ErrorAction Ignore 'Local Settings'
 }
 
+# ---------------------------------------
+# install functions
+# ---------------------------------------
+
+function hf_install_vscode_packages() {
+  echo "vscode packages"
+  $PKGS=""
+  # text edit
+  $PKGS+="mkloubert.vscode-remote-workspace henriiik.vscode-sort stkb.rewrap formulahendry.auto-close-tag "
+  # docker
+  # $PKGS+="peterjausovec.vscode-docker "
+  # cpp
+  $PKGS+="ms-vscode.cpptools twxs.cmake matthewferreira.cppcheck "
+  # python
+  $PKGS+="ms-python.python "
+  # html-css
+  $PKGS+="techer.open-in-browser mkaufman.HTMLHint "
+  # xml
+  $PKGS+="DotJoshJohnson.xml "
+  # javascript
+  $PKGS+="dbaeumer.vscode-eslint msjsdiag.debugger-for-chrome "
+  # python
+  $PKGS+="ms-python.python "
+  # typescript
+  $PKGS+="eg2.tslint "
+  # dart
+  $PKGS+="Dart-Code.dart-code "
+  # lua
+  $PKGS+="trixnz.vscode-lua satoren.lualint "
+  # java
+  # $PKGS+="redhat.java vscjava.vscode-java-pack "
+  $PKGS+="vscjava.vscode-java-pack "
+  # markdown
+  $PKGS+="mrmlnc.vscode-remark davidanson.vscode-markdownlint "
+  # latex
+  $PKGS+="James-Yu.latex-workshop "
+  # bash
+  $PKGS+="timonwong.shellcheck "
+  # powershell
+  $PKGS+="ms-vscode.powershell "
+
+  hf_vscode_install_packages $PKGS
+}
 
 function hf_choco_boostrap() {
     echo "boostrap windows env"
@@ -55,26 +111,64 @@ function hf_packages_store() {
   "Microsoft.XboxGameOverlay Microsoft.GetHelp Microsoft.XboxApp Microsoft.Xbox.TCUI Microsoft.XboxSpeechToTextOverlay Microsoft.Wallet Facebook.Facebook 9E2F88E3.Twitter Microsoft.MinecraftUWP A278AB0D.MarchofEmpires Microsoft.Messaging Microsoft.Appconnector Microsoft.BingNews Microsoft.SkypeApp Microsoft.BingSports Microsoft.CommsPhone Microsoft.ConnectivityStore Microsoft.Office.Sway Microsoft.WindowsPhone Microsoft.XboxIdentityProvider Microsoft.StorePurchaseApp Microsoft.DesktopAppInstaller Microsoft.BingWeather Microsoft.MicrosoftStickyNotes Microsoft.MicrosoftSolitaireCollection Microsoft.OneConnect Microsoft.People Microsoft.ZuneMusic Microsoft.ZuneVideo Microsoft.Getstarted Microsoft.XboxApp microsoft.windowscommunicationsapps Microsoft.WindowsCamera Microsoft.WindowsSoundRecorder Microsoft.WindowsMaps Microsoft.3DBuilder Microsoft.WindowsFeedbackHub Microsoft.MicrosoftOfficeHub Microsoft.WindowsAlarms Microsoft.3DBuilder Microsoft.OneDrive 89006A2E.AutodeskSketchBook A278AB0D.DisneyMagicKingdoms king.com.BubbleWitch3Saga king.com.CandyCrushSodaSaga Microsoft.Print3D".Split(" ") | Foreach{Get-AppxPackage -allusers $_ |remove-AppxPackage}
 }
 
-function hf_get_wifi_SSIDs() {
-    return (netsh wlan show net mode=bssid)
-}
-
-function hf_open_start-menu() {
-  explorer '%ProgramData%\Microsoft\Windows\Start Menu\Programs'
-}
-
 function hf_store_list_installed() {
   Get-AppxPackage -AllUsers | Select Name, PackageFullName
 }
 
-function hf_remove_ondrive() {
+# ---------------------------------------
+# network functions
+# ---------------------------------------
+
+function hf_choco_install_packages_essentials() {
+  echo "packages choco"
+  choco install -y --acceptlicense --no-progress firefox google-backup-and-sync visualstudiocode FoxitReader 7zip vlc ccleaner shellcheck git zotero-standalone libreoffice pdfsam
+  choco upgrade -y --acceptlicense --no-progress all
+}
+
+function hf_choco_install_packages_dev() {
+  echo "packages choco dev"
+  choco install -y --acceptlicense --no-progress unity-android msys2 cppcheck cmake nsis nodejs rufus git gitg dependencywalker
+}
+
+function hf_choco_install_packages_user() {
+  echo "packages choco user"
+  choco install -y --acceptlicense --no-progress battle.net steam deluge stremio
+}
+
+# ---------------------------------------
+# network functions
+# ---------------------------------------
+
+function hf_network_get_wifi_SSIDs() {
+  return (netsh wlan show net mode=bssid)
+}
+
+# ---------------------------------------
+# explorer functions
+# ---------------------------------------
+
+function hf_explorer_open_start_menu() {
+  explorer '%ProgramData%\Microsoft\Windows\Start Menu\Programs'
+}
+
+# ---------------------------------------
+# windows functions
+# ---------------------------------------
+
+
+function hf_windows_update() {
+  control update
+  wuauclt /detectnow /updatenow
+}
+
+function hf_windows_remove_ondrive() {
   echo "remove onedrive"
   cmd /c 'IF EXIST "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f'
   cmd /c 'IF EXIST "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f'
   cmd /c 'REG ADD "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v System.IsPinnedToNameSpaceTree /d "0" /t REG_DWORD /f'
 }
 
-function hf_context_menu_unused() {
+function hf_windows_disable_unused_menu() {
   echo "remove context menu unused"
   # * Sharing
   cmd /c 'IF EXIST "HKEY_CLASSES_ROOT\*\shellex\ContextMenuHandlers\ModernSharing" REG Delete "HKEY_CLASSES_ROOT\*\shellex\ContextMenuHandlers\ModernSharing" /f'
@@ -87,26 +181,9 @@ function hf_context_menu_unused() {
   cmd /c 'IF EXIST "HKEY_CLASSES_ROOT\Directory\shellex\ContextMenuHandlers\Sharing" REG Delete "HKEY_CLASSES_ROOT\Directory\shellex\ContextMenuHandlers\Sharing" /f'
 }
 
-function hf_packages_choco() {
-  echo "packages choco"
-  choco install -y --acceptlicense --no-progress firefox google-backup-and-sync visualstudiocode FoxitReader 7zip vlc ccleaner shellcheck git zotero-standalone libreoffice pdfsam
-  choco upgrade -y --acceptlicense --no-progress all
-}
-
-function hf_packages_choco_dev() {
-  echo "packages choco dev"
-  choco install -y --acceptlicense --no-progress msys2 cppcheck cmake nsis nodejs rufus git gitg dependencywalker
-}
-
-function hf_packages_choco_user() {
-  echo "packages choco user"
-  choco install -y --acceptlicense --no-progress battle.net steam deluge stremio
-}
-
-function hf_update() {
-  control update
-  wuauclt /detectnow /updatenow
-}
+# ---------------------------------------
+# config functions
+# ---------------------------------------
 
 function hf_config_install() {
   cp ~/gdrive/env/home/.bashrc ~/
@@ -126,10 +203,8 @@ function hf_config_backup() {
   cp ~/.vimrc ~/gdrive/env/home/
   cp ~/.gitconfig ~/gdrive/env/home/
   cp ~/.profile ~/gdrive/env/home/
-  cp ~/AppData/Roaming/Code/User/settings.json \
-    ~/gdrive/env/home/.config/Code/User/settings.json
-  cp ~/AppData/Roaming/Code/User/keybindings.json \
-    ~/gdrive/env/home/.config/Code/keybindings.json
+  cp ~/AppData/Roaming/Code/User/settings.json ~/gdrive/env/home/.config/Code/User/settings.json
+  cp ~/AppData/Roaming/Code/User/keybindings.json ~/gdrive/env/home/.config/Code/keybindings.json
   cp ~/.inputrc ~/gdrive/env/home/.inputrc
 }
 
