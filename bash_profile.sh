76406700005a71abeb40b76ac9f09f802ea1c355
Alan Guedes <alanlivio@gmail.com>
2018-11-08 03:10:15 -0200

    add hf_install_spotify

--- bash_profile.sh
+++ bash_profile.sh
@@ -948,6 +948,18 @@ function hf_install_chrome(){
   fi
 }
 
+function hf_install_spotify(){
+  hf_log_msg "install spotify"
+  dpkg --status spotify-client &> /dev/null
+  if test $? != 0; then
+    sudo rm /etc/apt/sources.list.d/spotify.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+    echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update
+    sudo apt-get install spotify-client
+  fi
+}
+
 function hf_install_grub_customizer(){
   hf_log_msg "install grub-customizer"
   dpkg --status grub-customizer &>/dev/null
