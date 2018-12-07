2e3d9c50d2c9088d0fd6ee627ab858066bce65d8
Alan Guedes <alanlivio@gmail.com>
2018-12-07 17:10:53 -0200

    use -y on apt

--- bash_profile.sh
+++ bash_profile.sh
@@ -129,7 +129,7 @@ function hf_mac_init() {
 }
 
 function hf_mac_enable_wifi() {
-  sudo apt install bcmwl-kernel-source
+  sudo apt install -y bcmwl-kernel-source
 }
 
 function hf_mac_install_homebrew(){
@@ -1040,7 +1040,7 @@ function hf_install_spotify(){
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
     echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
     sudo apt update
-    sudo apt install spotify-client
+    sudo apt install -y spotify-client
   fi
 }
 
