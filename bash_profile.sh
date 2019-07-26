d13e4a911d5984dbf1b2d55aacd6d992993ba413
Alan Guedes <alanlivio@gmail.com>
2019-07-25 22:38:20 -0300

    double quoute wildcard

--- bash_profile.sh
+++ bash_profile.sh
@@ -569,7 +569,7 @@ function hf_imagem_compress2() {
 # ---------------------------------------
 
 function hf_pdf_find_duplicates() {
-  find . -iname *.pdf -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find . -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
+  find . -iname "*.pdf" -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find . -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
 }
 
 function hf_pdf_remove_annotations() {
