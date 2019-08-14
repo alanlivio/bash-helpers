37dce5dfcaca98a4bf1e9c0720aed044cd050ab0
Alan Guedes <alanlivio@gmail.com>
2019-08-14 14:37:05 -0300

    update gnome_sanity

--- bash_profile.sh
+++ bash_profile.sh
@@ -932,6 +932,8 @@ function hf_gnome_reset_keybindings() {
 
 function hf_gnome_sanity() {
   hf_log_func
+  gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+  gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
   gsettings set org.gnome.desktop.background color-shading-type "solid"
   gsettings set org.gnome.desktop.background picture-uri ''
   gsettings set org.gnome.desktop.background primary-color "#000000"
