0f5860ebd0e327d6d2ef8e891d3084685e20e7ee
Alan Guedes <alanlivio@gmail.com>
2020-10-20 15:37:56 -0300

    add sh hf_pdf_concat

--- helpers.sh
+++ helpers.sh
@@ -853,6 +853,11 @@ function hf_imagem_compress_hard() {
 # pdf
 # ---------------------------------------
 
+function hf_pdf_concat() {
+  : ${2?"Usage: ${FUNCNAME[0]} <pdf_1> <pdf_2>"}
+  pdftk $1 $2 cat output ${1%.*}-concatenated.pdf
+}
+
 function hf_pdf_find_duplicates() {
   find . -iname "*.pdf" -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find . -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
 }
