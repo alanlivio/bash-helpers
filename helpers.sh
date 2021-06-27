029cbbcfb5844c86e32f8bd836e1dfe661fc9403
Alan Guedes <alanlivio@gmail.com>
2021-06-27 17:45:17 +0100

    sh refactory with linux-if

--- helpers.sh
+++ helpers.sh
@@ -70,28 +70,30 @@ function hf_test_command() {
 # setup functions
 # ---------------------------------------
 
-function hf_setup_gnome() {
-  hf_log_func
-  # sudo nopasswd
-  hf_user_permissions_sudo
-  # shell configure
-  hf_gnome_dark
-  hf_gnome_sanity
-  hf_gnome_disable_unused_apps_in_search
-  hf_gnome_disable_super_workspace_change
-  # cleanup
-  hf_home_clean_unused_dirs
-  # vim/git/essentials
-  PKGS="vim git diffutils curl wget bash deborphan apt-file net-tools zip "
-  # python
-  PKGS+="python3 python3-pip "
-  hf_apt_install_packages $PKGS
-  # set python3 as default
-  hf_python_set_python3_default
-  # install vscode
-  hf_install_vscode
-  hf_vscode_install_config_files
-}
+if test $IS_LINUX; then
+  function hf_setup_gnome() {
+    hf_log_func
+    # sudo nopasswd
+    hf_user_permissions_sudo
+    # shell configure
+    hf_gnome_dark
+    hf_gnome_sanity
+    hf_gnome_disable_unused_apps_in_search
+    hf_gnome_disable_super_workspace_change
+    # cleanup
+    hf_home_clean_unused_dirs
+    # vim/git/essentials
+    PKGS="vim git diffutils curl wget bash deborphan apt-file net-tools zip "
+    # python
+    PKGS+="python3 python3-pip "
+    hf_apt_install_packages $PKGS
+    # set python3 as default
+    hf_python_set_python3_default
+    # install vscode
+    hf_install_vscode
+    hf_vscode_install_config_files
+  }
+fi
 
 if test -n "$IS_WINDOWS"; then
   function hf_setup_wsl() {
@@ -150,26 +152,26 @@ fi
 # The following funcs requeres variables with PKGS_ prefix.
 # Such variables can be configured in .bashrc or helpers-cfg.sh.
 
-function hf_update_clean_gnome() {
-  # snap
-  hf_snap_install_packages $PKGS_SNAP
-  hf_snap_install_packages_classic $PKGS_SNAP_CLASSIC
-  hf_snap_upgrade
-  hf_apt_upgrade
-  # apt
-  hf_apt_install_packages $PKGS_APT
-  hf_apt_remove_packages $PKGS_REMOVE_APT
-  hf_apt_autoremove
-  hf_apt_remove_orphan_packages $PKGS_APT_ORPHAN_EXPECTIONS
-  # python
-  hf_python_install_packages $PKGS_PYTHON
-  # vscode
-  hf_vscode_install_packages $PKGS_VSCODE
-  # cleanup
-  hf_home_clean_unused_dirs
-}
-
-if test -n "$IS_WINDOWS"; then
+if test $IS_LINUX; then
+  function hf_update_clean_gnome() {
+    # snap
+    hf_snap_install_packages $PKGS_SNAP
+    hf_snap_install_packages_classic $PKGS_SNAP_CLASSIC
+    hf_snap_upgrade
+    hf_apt_upgrade
+    # apt
+    hf_apt_install_packages $PKGS_APT
+    hf_apt_remove_packages $PKGS_REMOVE_APT
+    hf_apt_autoremove
+    hf_apt_remove_orphan_packages $PKGS_APT_ORPHAN_EXPECTIONS
+    # python
+    hf_python_install_packages $PKGS_PYTHON
+    # vscode
+    hf_vscode_install_packages $PKGS_VSCODE
+    # cleanup
+    hf_home_clean_unused_dirs
+  }
+elif test -n "$IS_WINDOWS"; then
   function hf_update_clean_wsl() {
     # apt
     hf_apt_upgrade
@@ -210,9 +212,7 @@ if test -n "$IS_WINDOWS"; then
     hf_ps_call hf_home_hide_dotfiles
     hf_ps_call hf_explorer_clean_unused_shortcuts
   }
-fi
-
-if test -n "$IS_MAC"; then
+elif test -n "$IS_MAC"; then
   function hf_update_clean_mac() {
     # brew
     hf_brew_install $PKGS_BREW
@@ -314,8 +314,6 @@ fi
 # ---------------------------------------
 
 if test -n "$IS_WINDOWS_WSL"; then
-  # x,pulseaudio server
-
   function hf_wsl_x_pulseaudio_enable() {
     hf_ps_call_admin "hf_choco_install pulseaudio vcxsrv"
 
@@ -1915,173 +1913,148 @@ function hf_mount_list() {
 # gnome
 # ---------------------------------------
 
-function hf_gnome_execute_desktop_file() {
-  awk '/^Exec=/ {sub("^Exec=", ""); gsub(" ?%[cDdFfikmNnUuv]", ""); exit system($0)}' $1
-}
+if test $IS_LINUX; then
+  function hf_gnome_execute_desktop_file() {
+    awk '/^Exec=/ {sub("^Exec=", ""); gsub(" ?%[cDdFfikmNnUuv]", ""); exit system($0)}' $1
+  }
 
-function hf_gnome_reset_keybindings() {
-  hf_log_func
-  gsettings reset-recursively org.gnome.mutter.keybindings
-  gsettings reset-recursively org.gnome.mutter.wayland.keybindings
-  gsettings reset-recursively org.gnome.desktop.wm.keybindings
-  gsettings reset-recursively org.gnome.shell.keybindings
-  gsettings reset-recursively org.gnome.settings-daemon.plugins.media-keys
-}
+  function hf_gnome_reset_keybindings() {
+    hf_log_func
+    gsettings reset-recursively org.gnome.mutter.keybindings
+    gsettings reset-recursively org.gnome.mutter.wayland.keybindings
+    gsettings reset-recursively org.gnome.desktop.wm.keybindings
+    gsettings reset-recursively org.gnome.shell.keybindings
+    gsettings reset-recursively org.gnome.settings-daemon.plugins.media-keys
+  }
 
-function hf_gnome_dark() {
-  gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
-  gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-  gsettings set org.gnome.desktop.interface icon-theme 'ubuntu-mono-dark'
-}
+  function hf_gnome_dark() {
+    gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
+    gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+    gsettings set org.gnome.desktop.interface icon-theme 'ubuntu-mono-dark'
+  }
 
-function hf_gnome_sanity() {
-  hf_log_func
-  # gnome search
-  gsettings set org.gnome.desktop.search-providers sort-order "[]"
-  gsettings set org.gnome.desktop.search-providers disable-external false
-  gsettings set org.gnome.desktop.search-providers disabled "['org.gnome.Calculator.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.clocks.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Software.desktop']"
-  # animation
-  gsettings set org.gnome.desktop.interface enable-animations false
-  # background
-  gsettings set org.gnome.desktop.background color-shading-type "solid"
-  gsettings set org.gnome.desktop.background picture-uri ''
-  gsettings set org.gnome.desktop.background primary-color "#000000"
-  gsettings set org.gnome.desktop.background secondary-color "#000000"
-  gsettings set org.gnome.desktop.background show-desktop-icons false
-  # cloack
-  gsettings set org.gnome.desktop.interface clock-show-date true
-  # notifications
-  gsettings set org.gnome.desktop.notifications show-banners false
-  gsettings set org.gnome.desktop.notifications show-in-lock-screen false
-  # recent files
-  gsettings set org.gnome.desktop.privacy remember-recent-files false
-  # screensaver
-  gsettings set org.gnome.desktop.screensaver color-shading-type "solid"
-  gsettings set org.gnome.desktop.screensaver lock-enabled false
-  gsettings set org.gnome.desktop.screensaver picture-uri ''
-  gsettings set org.gnome.desktop.screensaver primary-color "#000000"
-  gsettings set org.gnome.desktop.screensaver secondary-color "#000000"
-  # sound
-  gsettings set org.gnome.desktop.sound event-sounds false
-  gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
-  # gedit
-  gsettings set org.gnome.gedit.preferences.editor bracket-matching true
-  gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
-  gsettings set org.gnome.gedit.preferences.editor display-right-margin true
-  gsettings set org.gnome.gedit.preferences.editor scheme 'classic'
-  gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode 'word'
-  gsettings set org.gnome.gedit.preferences.editor wrap-mode 'word'
-  # workspaces
-  gsettings set org.gnome.mutter dynamic-workspaces false
-  # nautilus
-  gsettings set org.gnome.nautilus.list-view default-zoom-level 'small'
-  gsettings set org.gnome.nautilus.list-view use-tree-view true
-  gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-  gsettings set org.gnome.nautilus.window-state maximized false
-  gsettings set org.gnome.nautilus.window-state sidebar-width 180
-  # dock
-  gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
-  gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
-  gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
-  gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
-  gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
-}
+  function hf_gnome_sanity() {
+    hf_log_func
+    # gnome search
+    gsettings set org.gnome.desktop.search-providers sort-order "[]"
+    gsettings set org.gnome.desktop.search-providers disable-external false
+    gsettings set org.gnome.desktop.search-providers disabled "['org.gnome.Calculator.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.clocks.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Software.desktop']"
+    # animation
+    gsettings set org.gnome.desktop.interface enable-animations false
+    # background
+    gsettings set org.gnome.desktop.background color-shading-type "solid"
+    gsettings set org.gnome.desktop.background picture-uri ''
+    gsettings set org.gnome.desktop.background primary-color "#000000"
+    gsettings set org.gnome.desktop.background secondary-color "#000000"
+    gsettings set org.gnome.desktop.background show-desktop-icons false
+    # cloack
+    gsettings set org.gnome.desktop.interface clock-show-date true
+    # notifications
+    gsettings set org.gnome.desktop.notifications show-banners false
+    gsettings set org.gnome.desktop.notifications show-in-lock-screen false
+    # recent files
+    gsettings set org.gnome.desktop.privacy remember-recent-files false
+    # screensaver
+    gsettings set org.gnome.desktop.screensaver color-shading-type "solid"
+    gsettings set org.gnome.desktop.screensaver lock-enabled false
+    gsettings set org.gnome.desktop.screensaver picture-uri ''
+    gsettings set org.gnome.desktop.screensaver primary-color "#000000"
+    gsettings set org.gnome.desktop.screensaver secondary-color "#000000"
+    # sound
+    gsettings set org.gnome.desktop.sound event-sounds false
+    gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+    # gedit
+    gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+    gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+    gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+    gsettings set org.gnome.gedit.preferences.editor scheme 'classic'
+    gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode 'word'
+    gsettings set org.gnome.gedit.preferences.editor wrap-mode 'word'
+    # workspaces
+    gsettings set org.gnome.mutter dynamic-workspaces false
+    # nautilus
+    gsettings set org.gnome.nautilus.list-view default-zoom-level 'small'
+    gsettings set org.gnome.nautilus.list-view use-tree-view true
+    gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+    gsettings set org.gnome.nautilus.window-state maximized false
+    gsettings set org.gnome.nautilus.window-state sidebar-width 180
+    # dock
+    gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+    gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
+    gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
+  }
 
-function hf_gnome_disable_unused_apps_in_search() {
-  hf_log_func
-  APPS_TO_HIDE=$(find /usr/share/applications/ -iname '*im6*' -iname '*java*' -o -iname '*JB*' -o -iname '*policy*' -o -iname '*icedtea*' -o -iname '*uxterm*' -o -iname '*display-im6*' -o -iname '*unity*' -o -iname '*webbrowser-app*' -o -iname '*amazon*' -o -iname '*icedtea*' -o -iname '*xdiagnose*' -o -iname yelp.desktop -o -iname '*brasero*')
-  for i in $APPS_TO_HIDE; do
-    sudo sh -c " echo 'NoDisplay=true' >> $i"
-  done
-}
+  function hf_gnome_disable_unused_apps_in_search() {
+    hf_log_func
+    APPS_TO_HIDE=$(find /usr/share/applications/ -iname '*im6*' -iname '*java*' -o -iname '*JB*' -o -iname '*policy*' -o -iname '*icedtea*' -o -iname '*uxterm*' -o -iname '*display-im6*' -o -iname '*unity*' -o -iname '*webbrowser-app*' -o -iname '*amazon*' -o -iname '*icedtea*' -o -iname '*xdiagnose*' -o -iname yelp.desktop -o -iname '*brasero*')
+    for i in $APPS_TO_HIDE; do
+      sudo sh -c " echo 'NoDisplay=true' >> $i"
+    done
+  }
 
-function hf_gnome_disable_super_workspace_change() {
-  hf_log_func
-  # remove super+arrow virtual terminal change
-  sudo sh -c 'dumpkeys |grep -v cr_Console |loadkeys'
-}
+  function hf_gnome_disable_super_workspace_change() {
+    hf_log_func
+    # remove super+arrow virtual terminal change
+    sudo sh -c 'dumpkeys |grep -v cr_Console |loadkeys'
+  }
 
-function hf_gnome_disable_tiling() {
-  # disable tiling
-  gsettings set org.gnome.mutter edge-tiling false
-}
+  function hf_gnome_disable_tiling() {
+    # disable tiling
+    gsettings set org.gnome.mutter edge-tiling false
+  }
 
-function hf_gnome_reset_tracker() {
-  sudo tracker reset --hard
-  sudo tracker daemon -s
-}
+  function hf_gnome_reset_tracker() {
+    sudo tracker reset --hard
+    sudo tracker daemon -s
+  }
 
-function hf_gnome_reset_shotwell() {
-  rm -r $HOME/.cache/shotwell $HOME/.local/share/shotwell
-}
+  function hf_gnome_reset_shotwell() {
+    rm -r $HOME/.cache/shotwell $HOME/.local/share/shotwell
+  }
 
-function hf_gnome_update_desktop_database() {
-  sudo update-desktop-database -v /usr/share/applications $HOME/.local/share/applications $HOME/.gnome/apps/
-}
+  function hf_gnome_update_desktop_database() {
+    sudo update-desktop-database -v /usr/share/applications $HOME/.local/share/applications $HOME/.gnome/apps/
+  }
 
-function hf_gnome_update_icons() {
-  sudo update-icon-caches -v /usr/share/icons/ $HOME/.local/share/icons/
-}
+  function hf_gnome_update_icons() {
+    sudo update-icon-caches -v /usr/share/icons/ $HOME/.local/share/icons/
+  }
 
-function hf_gnome_version() {
-  gnome-shell --version
-  mutter --version | head -n 1
-  gnome-terminal --version
-  gnome-text-editor --version
-}
+  function hf_gnome_version() {
+    gnome-shell --version
+    mutter --version | head -n 1
+    gnome-terminal --version
+    gnome-text-editor --version
+  }
 
-function hf_gnome_gdm_restart() {
-  sudo /etc/setup.d/gdm3 restart
-}
+  function hf_gnome_gdm_restart() {
+    sudo /etc/setup.d/gdm3 restart
+  }
 
-function hf_gnome_settings_reset() {
-  : ${1?"Usage: ${FUNCNAME[0]} <scheme>"}
-  gsettings reset-recursively $1
-}
+  function hf_gnome_settings_reset() {
+    : ${1?"Usage: ${FUNCNAME[0]} <scheme>"}
+    gsettings reset-recursively $1
+  }
 
-function hf_gnome_settings_save_to_file() {
-  : ${2?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
-  dconf dump $1 >$2
-}
+  function hf_gnome_settings_save_to_file() {
+    : ${2?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
+    dconf dump $1 >$2
+  }
 
-function hf_gnome_settings_load_from_file() {
-  : ${1?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
-  dconf load $1 <$2
-}
+  function hf_gnome_settings_load_from_file() {
+    : ${1?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
+    dconf load $1 <$2
+  }
 
-function hf_gnome_settings_diff_actual_and_file() {
-  : ${2?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
-  TMP_FILE=/tmp/gnome_settings_diff
-  hf_gnome_settings_save_to_file $1 $TMP_FILE
-  diff $TMP_FILE $2
-}
-
-# ---------------------------------------
-# vlc
-# ---------------------------------------
-
-function hf_vlc_youtube_playlist_extension() {
-  wget --continue https://dl.opendesktop.org/api/files/download/id/1473753829/149909-playlist_youtube.lua -P /tmp/
-  if test $? != 0; then hf_log_error "wget failed." && return 1; fi
-  sudo install /tmp/149909-playlist_youtube.lua /usr/lib/vlc/lua/playlist/
-}
-
-# ---------------------------------------
-# date
-# ---------------------------------------
-
-function hf_date() {
-  date +%F
-}
-
-# ---------------------------------------
-# openssl
-# ---------------------------------------
-
-function hf_opessl_showcerts_https() {
-  : ${1?"Usage: ${FUNCNAME[0]} <URL>"}
-  openssl s_client -showcerts -connect "$1":https
-}
+  function hf_gnome_settings_diff_actual_and_file() {
+    : ${2?"Usage: ${FUNCNAME[0]} <dconf-dir> <file_name>"}
+    TMP_FILE=/tmp/gnome_settings_diff
+    hf_gnome_settings_save_to_file $1 $TMP_FILE
+    diff $TMP_FILE $2
+  }
+fi
 
 # ---------------------------------------
 # system
@@ -2394,7 +2367,6 @@ function hf_install_windows_android_flutter() {
 # ---------------------------------------
 
 if test $IS_LINUX; then
-
   function hf_install_gnome_bb_warsaw() {
     hf_log_func
     if ! type warsaw &>/dev/null; then
