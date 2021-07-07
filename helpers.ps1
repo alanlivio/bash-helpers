f3acf9ea02e8c472e2de36df6ea6d7aaffdee327
Alan Guedes <alanlivio@gmail.com>
2021-07-07 22:00:32 +0100

    ps use winget for git and vscode

--- helpers.ps1
+++ helpers.ps1
@@ -1138,14 +1138,14 @@ function hf_install_wsl_ubuntu() {
 function hf_install_git() {
   Invoke-Expression $hf_log_func
   if (!(Get-Command 'git.exe' -ea 0)) {
-    choco install git --params "/NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration"
+    winget install git
   }
 }
 
 function hf_install_vscode() {
   Invoke-Expression $hf_log_func
   if (!(Get-Command 'code.exe' -ea 0)) {
-    choco install vscode --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders "
+    winget install vscode
   }
 }
 
