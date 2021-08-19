820e4d6a408f0c0bd0d8a096f938cb08c4a3289b
Alan Guedes <alanlivio@gmail.com>
2021-08-19 13:12:40 +0100

    move docker and msys code

--- ubuntu/apt.sh
+++ ubuntu/apt.sh
@@ -137,3 +137,18 @@ function bh_apt_fetch_install() {
   fi
   sudo dpkg -i /tmp/$apt_name
 }
+
+# ---------------------------------------
+# install
+# ---------------------------------------
+
+function bh_install_docker() {
+  bh_log_funch
+  sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
