d81fc2dd597154ccc22cf2aa2e550d23c7367c89
Alan Guedes <alanlivio@gmail.com>
2021-09-05 17:13:51 +0100

    update explorer.sh

--- win/explorer.sh
+++ win/explorer.sh
@@ -6,6 +6,26 @@ function bh_explorer_open_trash() {
   ps_call 'Start-Process explorer shell:recyclebinfolder'
 }
 
+function bh_explorer_open_appdata_local_programns() {
+  ps_call 'Start-Process explorer "$env:localappdata\Programs"'
+}
+
+function bh_explorer_open_appdata() {
+  ps_call 'Start-Process explorer "$env:appdata"'
+}
+
+function bh_explorer_open_tmp() {
+  ps_call 'Start-Process explorer "$env:localappdata\temp"'
+}
+
+function bh_explorer_open_start_menu_folder() {
+  ps_call 'Start-Process explorer "$env:appdata\Microsoft\Windows\Start Menu\Programs"'
+}
+
+function bh_explorer_open_start_menu_folder_allusers() {
+  ps_call 'Start-Process explorer "$env:allusersprofile\Microsoft\Windows\Start Menu\Programs"'
+}
+
 function bh_explorer_restart() {
   ps_call 'taskkill /f /im explorer.exe | Out-Null'
   ps_call 'Start-Process explorer.exe'
@@ -15,7 +35,7 @@ function bh_explorer_restore_desktop() {
   ps_call '
     if (Test-Path "${env:userprofile}\Desktop") { return}
     mkdir "${env:userprofile}\Desktop"
-    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop" /t REG_SZ /d "C:\Users\${env:username}\Desktop" /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop" /t REG_SZ /d "${env:userprofile}\Desktop" /f
     reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Desktop" /t REG_EXPAND_SZ /d "${env:userprofile}\Desktop" /f
     attrib +r -s -h "${env:userprofile}\Desktop"
   '
