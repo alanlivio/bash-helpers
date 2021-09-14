3a9bd4ed17796846df861462b4ecfee4c1663e7c
Alan Guedes <alanlivio@gmail.com>
2021-09-14 09:43:23 +0100

    update website deploy

--- website/src/index-from-readme.js
+++ website/src/index-from-readme.js
@@ -10,7 +10,6 @@ title: "bash-helpers"
 hide_title: true
 ---
 ${readme_without_img}`
-
   fs.writeFile('./src/pages/index.md', content, function (err) {
     if (err) return console.log(err);
   });
