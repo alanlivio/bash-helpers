298b88be2794dc94e40ed3f94bf57438e71e63ae
Alan Guedes <alanlivio@gmail.com>
2021-10-04 10:10:33 +0100

    update youtube-dl

--- lib/youtube-dl.sh
+++ lib/youtube-dl.sh
@@ -14,7 +14,7 @@ function bh_youtubedl_video480() {
   youtube-dl "$1" -f 'best[height<=480]' $YOUTUBEDL_PARAMS
 }
 
-function bh_youtubedl_video480_from_txt() {
+function bh_youtubedl_from_txt_video480() {
   : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
   youtube-dl -a "$1" -f 'best[height<=480]' $YOUTUBEDL_PARAMS
 }
@@ -24,7 +24,7 @@ function bh_youtubedl_audio() {
   youtube-dl "$1" $YOUTUBEDL_PARAMS -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --ignore-errors --embed-thumbnail --output "%(title)s.%(ext)s" --metadata-from-title "%(artist)s - %(title)s" --add-metadata
 }
 
-function bh_youtubedl_audio_best_from_txt() {
+function bh_youtubedl_from_txt_audio() {
   : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
   youtube-dl -a "$1" $YOUTUBEDL_PARAMS -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --ignore-errors --embed-thumbnail --output "%(title)s.%(ext)s" --metadata-from-title "%(artist)s - %(title)s" --add-metadata
 }
