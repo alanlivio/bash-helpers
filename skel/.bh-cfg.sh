bce3ae012c96821558f1fb8ec35a679b1971085e
Alan Guedes <alanlivio@gmail.com>
2022-01-05 16:58:18 +0000

    update skel/.bh-cfg.sh

--- skel/.bh-cfg.sh
+++ skel/.bh-cfg.sh
@@ -2,15 +2,23 @@
 # init vars
 # ----------------------------------------
 # ubuntu-only
-BH_PKGS_SNAP="" BH_PKGS_SNAP_CLASSIC="" BH_PKGS_APT_UBUNTU=""
+BH_PKGS_SNAP=""
+BH_PKGS_SNAP_CLASSIC=""
+BH_PKGS_APT_UBUNTU=""
 # linux-wsl-only
-BH_PKGS_APT_WSL="" BH_PKGS_APT_REMOVE_WSL=""
+BH_PKGS_APT_WSL=""
+BH_PKGS_APT_REMOVE_WSL=""
 # win-only
-BH_PKGS_MSYS="" BH_PKGS_CHOCO="" BH_PKGS_WINGET="" BH_PKGS_PYTHON_MSYS=""
+BH_PKGS_MSYS=""
+BH_PKGS_CHOCO=""
+BH_PKGS_WINGET=""
+BH_PKGS_PYTHON_MSYS=""
 # mac-only
 BH_PKGS_BREW=""
 # others
-BH_PKGS_VSCODE="" BH_PKGS_NPM="" BH_PKGS_PYTHON=""
+BH_PKGS_VSCODE=""
+BH_PKGS_NPM=""
+BH_PKGS_PYTHON=""
 
 # ----------------------------------------
 # pkgs
@@ -33,6 +41,11 @@ BH_PKGS_PYTHON+="nbdime " # jypter diff
 BH_PKGS_PYTHON+="autopep8 pylama pylint "
 BH_PKGS_VSCODE+="ms-python.python "
 
+if $IS_WINDOWS_GITBASH; then
+  BH_PKGS_VSCODE+="ms-vscode.powershell "
+  BH_PKGS_VSCODE+="ms-vscode-remote.remote-wsl "
+fi
+
 # ----------------------------------------
 # home dev folder
 # ----------------------------------------
@@ -40,7 +53,6 @@ BH_DEV="$HOME/dev"
 # BH_DEV_REPOS="parent/folder/name1/in/BH_DEV <REPO_URL_1>"
 # BH_DEV_REPOS="parent/folder/name1/in/BH_DEV <REPO_URL_2>"
 # BH_DEV_REPOS="parent/folder/name2/in/BH_DEV <REPO_URL_3>"
-
 
 # ----------------------------------------
 # home clean unused
