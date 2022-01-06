d6578717b48114e9dae094ca0c0c4ddea340e3af
Alan Guedes <alanlivio@gmail.com>
2022-01-06 12:23:32 +0000

    update mover user.shto  inside gitbash.sh

--- lib/rc-gitbash.sh
+++ lib/rc-gitbash.sh
@@ -41,13 +41,68 @@ function ps_call_script_admin() {
 }
 
 # ---------------------------------------
+# user
+# ---------------------------------------
+
+# usage if [ "$(bh_win_user_check_admin)" == "True" ]; then <commands>; fi
+function bh_win_user_check_admin() {
+  ps_call '
+    $user = "$env:COMPUTERNAME\$env:USERNAME"
+    $group = "Administrators"
+    (Get-LocalGroupMember $group).Name -contains $user
+  '
+}
+
+function bh_win_user_check_eleveated_shell() {
+  ps_call '(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)' | tr -d '\rn'
+}
+
+# ---------------------------------------
+# path
+# ---------------------------------------
+
+function bh_win_path_show() {
+  ps_call 'Get-ChildItem Env:'
+}
+
+function bh_win_path_show_as_list() {
+  IFS=';' read -ra ADDR <<<$(bh_win_path_show)
+  for i in "${!ADDR[@]}"; do echo ${ADDR[$i]}; done
+}
+
+function bh_win_env() {
+  ps_call "[System.Environment]::SetEnvironmentVariable('$1', '$2', 'user')"
+}
+
+function bh_win_path_add() {
+  local dir=$(winpath $@)
+  echo $dir
+  ps_call ' 
+    function bh_win_path_add($addPath) {
+      if (Test-Path $addPath) {
+        $currentpath = [System.Environment]::GetEnvironmentVariable("PATH", "user")
+        $regexAddPath = [regex]::Escape($addPath)
+        $arrPath = $currentpath -split ";" | Sort-Object -Unique | Where-Object { $_ -notMatch "^$regexAddPath\\?" }
+        $newpath = ($arrPath + $addPath) -join ";"
+        [System.Environment]::SetEnvironmentVariable("PATH", $newpath, "user")
+      }
+      else {
+        Throw "$addPath is not a valid path."
+      }
+    }; bh_win_path_add ' \"$dir\"
+}
+
+function bh_win_path_settings() {
+  rundll32 sysdm.cpl,EditEnvironmentVariables
+}
+
+# ---------------------------------------
 # load commands
 # ---------------------------------------
 
 if type gsudo &>/dev/null; then source "$BH_DIR/lib/win/admin.sh"; fi
 source "$BH_DIR/lib/win/explorer.sh"
 source "$BH_DIR/lib/win/install.sh"
-source "$BH_DIR/lib/win/user.sh"
 source "$BH_DIR/lib/win/winget.sh"
 
 function bh_win_sanity() {
