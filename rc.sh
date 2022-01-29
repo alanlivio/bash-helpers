0b6fc2a47387ebc80bbc9f78aedac1d727534670
Alan Guedes <alanlivio@gmail.com>
2022-01-29 11:33:24 +0000

    rename essentials.sh to base.sh

--- rc.sh
+++ rc.sh
@@ -36,86 +36,15 @@ Darwin)
 esac
 
 # ---------------------------------------
-# vars/alias
+# load helpers
 # ---------------------------------------
-
 BH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BH_RC="$BH_DIR/rc.sh"
 # if not "$HOME/.bh-cfg.sh" copy skel
 if ! test -f "$HOME/.bh-cfg.sh"; then cp $BH_DIR/skel/.bh-cfg.sh $HOME/; fi
 # load from from .bh-cfg.sh
 source $HOME/.bh-cfg.sh
-# set some var if .bh-cfg do not.
-if test -z "$BH_OPT_WIN"; then BH_OPT_WIN="$HOME/AppData/Local/Programs"; fi
-if test -z "$BH_OPT_LINUX"; then BH_OPT_LINUX="$HOME/opt"; fi
-if test -z "$BH_DEV"; then BH_DEV="$HOME/dev"; fi
-
-if $IS_WIN; then
-  # hide windows user files when ls home
-  alias ls='ls --color=auto --hide=ntuser* --hide=NTUSER* --hide=AppData --hide=IntelGraphicsProfiles* --hide=MicrosoftEdgeBackups'
-fi
-
-# ---------------------------------------
-# essentials helpers
-# ---------------------------------------
-
-source "$BH_DIR/lib/essentials.sh" # uses echo, test, md5, curl, tar, unzip, curl, rename, find
-
-# ---------------------------------------
-# bh helpers
-# ---------------------------------------
-
-function bh_bh_update_from_github_and_reload() {
-  bh_log_func
-  cd $BH_DIR && git pull && cd $OLDPWD
-  bh_bashrc_reload
-}
-
-function bh_bh_install() {
-  bh_log_func
-  local line='source $HOME/.bh/rc.sh'
-  if ! grep -Fxq "$line" $HOME/.bashrc; then
-    echo -e "$line\n" >>$HOME/.bashrc
-  fi
-}
-
-# ---------------------------------------
-# specifc-commands helpers
-# ---------------------------------------
-
-if type code &>/dev/null; then
-  HAS_VSCODE=true
-  source "$BH_DIR/lib/cross/vscode.sh"
-fi
-if type pip &>/dev/null; then
-  HAS_PY=true
-  source "$BH_DIR/lib/cross/python.sh"
-fi
-
-if type adb &>/dev/null; then source "$BH_DIR/lib/cross/adb.sh"; fi
-if type cmake &>/dev/null; then source "$BH_DIR/lib/cross/cmake.sh"; fi
-if type docker &>/dev/null; then source "$BH_DIR/lib/cross/docker.sh"; fi
-if type ffmpeg &>/dev/null; then source "$BH_DIR/lib/cross/ffmpeg.sh"; fi
-if type flutter &>/dev/null; then source "$BH_DIR/lib/cross/flutter.sh"; fi
-if type gcc &>/dev/null; then source "$BH_DIR/lib/cross/gcc.sh"; fi
-if type ghostscript &>/dev/null; then source "$BH_DIR/lib/cross/ghostscript.sh"; fi
-if type git &>/dev/null; then source "$BH_DIR/lib/cross/git.sh"; fi
-if type gst-launch-1.0 &>/dev/null; then source "$BH_DIR/lib/cross/gst.sh"; fi
-if type meson &>/dev/null; then source "$BH_DIR/lib/cross/meson.sh"; fi
-if type pandoc &>/dev/null; then source "$BH_DIR/lib/cross/pandoc.sh"; fi
-if type pngquant &>/dev/null; then source "$BH_DIR/lib/cross/pngquant.sh"; fi
-if type pdflatex &>/dev/null; then source "$BH_DIR/lib/cross/pdflatex.sh"; fi
-if type pkg-config &>/dev/null; then source "$BH_DIR/lib/cross/pkg-config.sh"; fi
-if type ruby &>/dev/null; then source "$BH_DIR/lib/cross/ruby.sh"; fi
-if type ssh &>/dev/null; then source "$BH_DIR/lib/cross/ssh.sh"; fi
-if type tesseract &>/dev/null; then source "$BH_DIR/lib/cross/tesseract.sh"; fi
-if type wget &>/dev/null; then source "$BH_DIR/lib/cross/wget.sh"; fi
-if type youtube-dl &>/dev/null; then source "$BH_DIR/lib/cross/youtube-dl.sh"; fi
-if type zip tar &>/dev/null; then source "$BH_DIR/lib/cross/zip.sh"; fi
-
-# ---------------------------------------
-# specific-OS helpers
-# ---------------------------------------
+source "$BH_DIR/lib/base.sh" # uses echo, test, md5, curl, tar, unzip, curl, rename, find
 
 if $IS_LINUX_UBU; then
   source "$BH_DIR/lib/rc-ubu.sh"
