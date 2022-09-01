820fbad5b377f0d100fab1adc3376b659b1fa610
Alan Guedes <alanlivio@gmail.com>
2022-09-01 15:02:22 +0100

    update youtube-dl

--- lib/commands/youtube-dl.bash
+++ lib/commands/youtube-dl.bash
@@ -1,26 +1,16 @@
-YOUTUBEDL_ARGS="--download-archive .downloaded.txt --no-warnings --no-post-overwrites --ignore-errors --prefer-free-formats --merge-output-format webm "
+YOUTUBEDL_ARGS="--download-archive .downloaded.txt --no-warnings --add-metadata --no-post-overwrites --ignore-errors "
 
-function youtubedl_from_txt() {
-  : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
-  youtube-dl -a "$1" --download-archive .downloaded.txt $YOUTUBEDL_ARGS
-}
-
-function youtubedl_video480() {
+function youtube-dl_video480() {
   : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
   youtube-dl "$1" -f 'best[height<=480]' $YOUTUBEDL_ARGS
 }
 
-function youtubedl_from_txt_video480() {
+function youtube-dl_video480_from_txt() {
   : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
   youtube-dl -a "$1" -f 'best[height<=480]' $YOUTUBEDL_ARGS
 }
 
-function youtubedl_audio() {
+function youtube-dl_audio() {
   : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
-  youtube-dl "$1" $YOUTUBEDL_ARGS --extract-audio --audio-format aac --ignore-errors --embed-thumbnail --output "%(title)s.%(ext)s" --add-metadata
-}
-
-function youtubedl_from_txt_audio() {
-  : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
-  youtube-dl -a "$1" $YOUTUBEDL_ARGS --extract-audio --audio-format aac --ignore-errors --embed-thumbnail --output "%(title)s.%(ext)s" --add-metadata
+  youtube-dl "$1" $YOUTUBEDL_ARGS --extract-audio --embed-thumbnail --audio-quality 7 --output "%(title)s.%(ext)s" --add-metadata
 }
