6b186cc1f30c5be504c7486cdef9ca8a0ef4e4bb
Alan Guedes <alanlivio@gmail.com>
2022-09-06 17:52:11 +0100

    add ruby_jekyll_*

--- lib/commands/ruby.bash
+++ lib/commands/ruby.bash
@@ -7,3 +7,15 @@ function ruby_clean_local() {
     rm -rf vendor/cache/
     rm -rf Gemfile.lock
 }
+
+function ruby_jekyll_install_essentials() {
+    # dev used
+    sudo apt install zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev imagemagick
+    # main
+    sudo apt install ruby-dev ruby-rubygems ruby-bundler
+}
+
+function ruby_jekyll_serve() {
+    bundle install
+    bundle exec jekyll serve --watch
+}
