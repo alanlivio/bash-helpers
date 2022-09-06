d8c7c77ef090d848f424f6da317b92296baf7024
Alan Guedes <alanlivio@gmail.com>
2022-09-06 17:32:37 +0100

    add ruby_clean_local

--- lib/commands/ruby.bash
+++ lib/commands/ruby.bash
@@ -1,1 +1,9 @@
 alias ruby_install='gem install '
+
+function ruby_clean_local() {
+    rm -rf ~/.bundle/ ~/.gem/
+    rm -rf $GEM_HOME/bundler/ $GEM_HOME/cache/bundler/
+    rm -rf .bundle/
+    rm -rf vendor/cache/
+    rm -rf Gemfile.lock
+}
