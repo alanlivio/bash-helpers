b15e48d1418af39688c1fa8ef27a9e86e020f274
Alan Guedes <alanlivio@gmail.com>
2022-09-13 18:25:31 +0100

    update pandoc.bash

--- lib/pandoc.bash
+++ lib/pandoc.bash
@@ -1,4 +1,9 @@
-function pandoc_convert_to_markdown() {
+function pandoc_from_any_to_markdown() {
   : ${1?"Usage: ${FUNCNAME[0]} <file_name>"}
   pandoc -s $1 -t markdown -o ${1%.*}.md
+}
+
+function pandoc_from_docx_to_latex() {
+  : ${1?"Usage: ${FUNCNAME[0]} <file_name>"}
+  pandoc -s $1 -f docx -t latex -o ${1%.*}.tex
 }
