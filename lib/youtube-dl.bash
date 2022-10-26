d7fb75a9f05fbf88d8008f31427d9ab2a9a502fc
Alan Guedes <alanlivio@gmail.com>
2022-10-26 12:29:21 +0100

    add youtube-dl_sub_en_one_video

--- lib/youtube-dl.bash
+++ lib/youtube-dl.bash
@@ -1,5 +1,11 @@
 YOUTUBEDL_ARGS="--download-archive .downloaded.txt --no-warnings --add-metadata --no-post-overwrites --ignore-errors "
 
+function youtube-dl_sub_en_one_video() {
+  : ${1?"Usage: ${FUNCNAME[0]} <uld>"}
+  local url=${1%%&*} # remove all after & (to leave only www.youtube.com/watch?v=...)
+  youtube-dl --write-sub --skip-download --sub-langs en "$url"
+}
+
 function youtube-dl_video480() {
   : ${1?"Usage: ${FUNCNAME[0]} <uld>"}
   youtube-dl "$1" -f 'best[height<=480]' $YOUTUBEDL_ARGS
