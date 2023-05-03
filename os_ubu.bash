b4aae0814689fd2938eb615545a848cd3c721f95
Alan Guedes <alanlivio@outlook.com>
2023-05-03 20:02:06 +0100

    add apt_update_upgrade_autoremove

--- os_ubu.bash
+++ os_ubu.bash
@@ -15,14 +15,10 @@ function user_sudo_nopasswd() {
 # apt
 #########################
 
-alias apt_upgrade="sudo apt -y update; sudo apt -y upgrade"
-alias apt_update="sudo apt -y update"
+alias apt_update_upgrade_autoremove="sudo apt -y update;sudo apt -y upgrade;sudo apt -y autoremove"
 alias apt_ppa_remove="sudo add-apt-repository --remove"
 alias apt_ppa_list="apt policy"
-alias apt_install="sudo apt install -y"
 alias apt_autoremove="sudo apt -y autoremove"
-alias apt_clean=apt_autoremove
-alias apt_uninstall="sudo apt remove -y --purge "
 
 function apt_fixes() {
   sudo dpkg --configure -a
