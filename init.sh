baab4df78ac2bf10692b0a281e0cbaf68859606d
Alan Guedes <alanlivio@gmail.com>
2023-06-28 17:32:41 +0100

    fix init.sh loading

--- init.sh
+++ init.sh
@@ -9,15 +9,15 @@ BH_DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "$BH_DIR/os_any.bash"
 
 if [[ $OSTYPE == msys* || -n $WSL_DISTRO_NAME ]]; then
-  echo source "$BH_DIR/os_win.bash";
+  source "$BH_DIR/os_win.bash";
 fi
 
 if [[ $OSTYPE == linux* ]]; then
-  echo source "$BH_DIR/os_ubu.bash"
+  source "$BH_DIR/os_ubu.bash"
 fi
 
 if [[ $OSTYPE == darwin* ]]; then
-  echo source "$BH_DIR/os_mac.bash"
+  "$BH_DIR/os_mac.bash"
 fi
 
 
