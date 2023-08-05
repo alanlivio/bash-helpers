95e947ff59f0077e0fa7ab0185a7cbe6898e5b90
Alan Guedes <alanlivio@outlook.com>
2023-08-05 23:40:28 +0100

    add convert_pptx_compress

--- commands/convert.bash
+++ commands/convert.bash
@@ -5,3 +5,25 @@ function convert_heic_to_jpg_at_dir() {
     log_msg "no .heic file at dir"
   fi
 }
+
+
+function convert_pptx_compress(){
+  [[ -d xtractd ]] && rm -r xtractd
+  unzip "$1" -d xtractd
+  cd xtractd/ppt/media
+  mogrify -resize 70% *.png
+  mogrify -resize 70% *.jpeg
+  mogrify -resize 70% *.gif
+  cd ..
+  # fix linked files
+  sed -i '' -e 's/\(Target="[^"]*\)tiff"/\1png"/g' slides/_rels/*.rels
+  sed -i '' -e 's/\(Target="[^"]*\)tiff"/\1gif"/g' slides/_rels/*.rels
+  sed -i '' -e 's/\(Target="[^"]*\)tiff"/\1jpeg"/g' slides/_rels/*.rels
+  cd ..
+  # create file
+  rm "../$1"
+  zip -r "../$1" *
+  cd ..
+  # remove uncompressed folder
+  rm -r xtractd
+}
