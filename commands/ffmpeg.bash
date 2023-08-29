7bc8277b02d5a416a170770076a28f4a86200823
Alan Guedes <alanlivio@outlook.com>
2023-08-29 13:48:01 +0100

    add ffmpeg_extract_audio_mp4

--- commands/ffmpeg.bash
+++ commands/ffmpeg.bash
@@ -3,6 +3,11 @@ function ffmpeg_cut_mp4() {
     ffmpeg -i $1 -vcodec copy -acodec copy -ss $2 -t $3 -f mp4 cuted-$1
 }
 
+function ffmpeg_extract_audio_mp4() {
+    : ${1?"Usage: ${FUNCNAME[0]} <video>"}
+    ffmpeg -i "$1" -vn -acodec copy "${1%.*}.m4a"
+}
+
 function ffmpeg_show_motion_vectors() {
     : ${1?"Usage: ${FUNCNAME[0]} <video>"}
     ffplay -flags2 +export_mvs -vf codecview=mv=pf+bf+bb $1
