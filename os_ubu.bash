d7fa7667ff9a478eb2b5b1d27a2246e166390b3b
Alan Guedes <alanlivio@outlook.com>
2023-08-31 10:36:29 +0100

    add ubu_install_wsl_cuda_11

--- os_ubu.bash
+++ os_ubu.bash
@@ -59,6 +59,21 @@ function deb_install_file_from_url() {
 }
 
 #########################
+# cuda
+#########################
+
+function ubu_install_wsl_cuda_11() {
+    # https://ubuntu.com/tutorials/enabling-gpu-acceleration-on-ubuntu-on-wsl2-with-the-nvidia-cuda-platform#3-install-nvidia-cuda-on-ubuntu
+    sudo apt-key del 7fa2af80
+    wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+    sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/3bf863cc.pub
+    sudo add-apt-repository 'deb https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/ /'
+    sudo apt-get update
+    sudo apt-get -y install cuda-11-8
+}
+
+#########################
 # snap
 #########################
 
