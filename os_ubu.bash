e73c3b85bbbde6f1c4237cf2fd5a319509d0c1e1
Alan Guedes <alanlivio@outlook.com>
2023-09-05 16:52:03 +0100

    update ubu_install_wsl_cuda_11

--- os_ubu.bash
+++ os_ubu.bash
@@ -64,11 +64,10 @@ function deb_install_file_from_url() {
 
 function ubu_install_wsl_cuda_11() {
     # https://ubuntu.com/tutorials/enabling-gpu-acceleration-on-ubuntu-on-wsl2-with-the-nvidia-cuda-platform#3-install-nvidia-cuda-on-ubuntu
+    # how fix gpg key: https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
     sudo apt-key del 7fa2af80
-    wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-    sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/3bf863cc.pub
-    sudo add-apt-repository 'deb https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/ /'
+    wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.0-1_all.deb
+    sudo dpkg -i cuda-keyring_1.0-1_all.deb
     sudo apt-get update
     sudo apt-get -y install cuda-11-8
 }
