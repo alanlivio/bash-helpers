bad61a93aae5bedc61b0c27e8e245b99d0cc54f4
Alan Guedes <alanlivio@outlook.com>
2023-09-24 09:49:12 +0100

    update yt-dlp

--- commands/yt-dlp.bash
+++ commands/yt-dlp.bash
@@ -1,7 +1,9 @@
+BATCH_EXTRA_ARGS="--download-archive .downloaded.txt --no-playlist"
+
 function yt_dlp_sub_en() {
     : ${1?"Usage: ${FUNCNAME[0]} <url to video>"}
     local url=${1%%&*} # remove all after & (to leave only www.youtube.com/watch?v=...)
-    yt-dlp --write-sub --skip-download --no-playlist --sub-langs en "$url"
+    yt-dlp --skip-download --write-sub --sub-langs en "$url"
 }
 
 function yt_dlp_audio() {
@@ -11,7 +13,7 @@ function yt_dlp_audio() {
 
 function yt_dlp_audio_from_txt() {
     : ${1?"Usage: ${FUNCNAME[0]} <url to audio or list>"}
-    yt-dlp --extract-audio --audio-format m4a --batch-file "$1"
+    yt-dlp --extract-audio --audio-format m4a --batch-file "$1" $BATCH_EXTRA_ARGS
 }
 
 function yt_dlp_video_480() {
@@ -21,5 +23,5 @@ function yt_dlp_video_480() {
 
 function yt_dlp_video_480_from_txt() {
     : ${1?"Usage: ${FUNCNAME[0]} <txt_file>"}
-    yt-dlp -f 'best[height<=480]' --recode-video mp4 --batch-file "$1"
+    yt-dlp -f 'best[height<=480]' --recode-video mp4 --batch-file "$1" $BATCH_EXTRA_ARGS
 }
