5a2f6e65f82ab7b239fc6a6ead2bf1f8b54224ec
Alan Guedes <alanlivio@outlook.com>
2023-10-07 13:05:05 +0100

    update latex_clean

--- os_any.bash
+++ os_any.bash
@@ -55,7 +55,7 @@ alias dotfiles_diff="_dotfiles_func diff"
 #########################
 
 function latex_clean() {
-    rm -rf comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf
+    rm -rf comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf ./*.aux ./*.fls ./*.fdb_latexmk ./*.log
 }
 
 #########################
