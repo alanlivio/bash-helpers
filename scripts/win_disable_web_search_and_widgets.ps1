1c13b51a9f2a569da635872b117ba2b7e20557d6
Alan Guedes <alanlivio@outlook.com>
2023-11-13 20:30:27 +0000

    update win_disabel_web_search_and_widgets

--- scripts/win_disable_web_search_and_widgets.ps1
+++ scripts/win_disable_web_search_and_widgets.ps1
@@ -1,4 +1,11 @@
 function log_msg() { Write-Host -ForegroundColor DarkYellow "--" ($args -join " ") }
 
-log_msg "disable Web Search and Web Widgets"
+
+log_msg "disable Web Search"
+New-PSDrive -Name HKCU -PSProvider Registry -Root HKEY_CURRENT_USER  -ea 0 | Out-Null
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name 'BingSearchEnabled' -Type DWORD -Value '0'
+New-Item -Path "HKCU:HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name 'DisableSearchBoxSuggestions' -Type DWORD -Value '1'
+
+log_msg "disable Web Widgets"
 winget.exe uninstall MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy
