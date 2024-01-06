4fa649284041e7f21bc5ffeddcf32f6350e0cc91
Alan Guedes <alanlivio@outlook.com>
2024-01-06 21:59:29 +0000

    fix win_disable_password_policy

--- os_win.ps1
+++ os_win.ps1
@@ -203,13 +203,13 @@ function win_disable_protocol_execute_warning() {
 }
 
 function win_disable_password_policy() {
-    $tmpfile = New-TemporaryFile
     gsudo {
+        $tmpfile = New-TemporaryFile
         secedit /export /cfg $tmpfile /quiet
         (Get-Content $tmpfile).Replace("PasswordComplexity = 1", "PasswordComplexity = 0").Replace("MaximumPasswordAge = 42", "MaximumPasswordAge = -1") | Out-File $tmpfile
         secedit /configure /db "$env:SYSTEMROOT\security\database\local.sdb" /cfg $tmpfile /areas SECURITYPOLICY | Out-Null
+        Remove-Item -Path $tmpfile
     }
-    Remove-Item -Path $tmpfile
 }
 
 function win_disable_web_search_and_widgets() {
