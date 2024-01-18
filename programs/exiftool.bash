60d3119b18e37bf2d8f819ccf0c8d1e133ce322b
Alan Guedes <alanlivio@outlook.com>
2024-01-18 12:02:58 +0000

    add exiftool.bash

---
+++ programs/exiftool.bash
@@ -0,0 +1,5 @@
+function exiftool_set_media_all_dates_to_one_year() {
+    : ${3?"Usage: ${FUNCNAME[0]} <year>"}
+    files=$(find . -iname "*.jpg" -o -iname "*.JPG" -o -iname "*.mp4" -o -iname "*.MP4")
+    exiftool -m -AllDates="$1:01:01 00:00:00"
+}
