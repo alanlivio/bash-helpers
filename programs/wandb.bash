c828a40711e06f3f9aa894838cad47c178a23f33
Alan Guedes <alanlivio@outlook.com>
2024-03-12 18:36:54 +0000

    add wandb_del_all_runs_from_project

--- programs/wandb.bash
+++ programs/wandb.bash
@@ -1,3 +1,16 @@
 function wandb_clean() {
     wandb sync --clean-force
 }
+
+function wandb_del_all_runs_from_project() {
+    : ${2?"Usage: ${FUNCNAME[0]} <entity_name> <project_name>"}
+    python - <<-EOF
+import wandb
+api = wandb.Api()
+runs = api.runs('$1/$2')
+for run in runs:
+    for artifact in run.logged_artifacts():
+        artifact.delete()
+    run.delete()
+EOF
+}
