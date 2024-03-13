34d01380c7a6ce5b781ad8a2bd35512426340a26
Alan Guedes <alanlivio@outlook.com>
2024-03-13 10:46:59 +0000

    update python.bash

--- programs/python.bash
+++ programs/python.bash
@@ -2,12 +2,17 @@ alias python_clean_cache='find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xar
 
 function pip_install() {
     for pkg in $@; do
-        pip show $pkg >/dev/null || pip install -U  $pkg
+        pip show $pkg >/dev/null || pip install -U $pkg
     done
 }
 
 function python_check_tensorflow() {
     python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+}
+
+function python_check_numa() {
+    type -p numactl >/dev/null || sudo apt-get install numactl
+    numactl --show
 }
 
 function python_packaging_install_local() {
@@ -18,6 +23,7 @@ function python_packaging_install_local() {
 }
 
 function python_packaging_upload_testpypi() {
+    type -p numactl >/dev/null || pip install twine
     [[ -d dist ]] && rm -r dist
     [[ -d build ]] && rm -r dist
     rm -rf ./*.egg-info
@@ -27,6 +33,7 @@ function python_packaging_upload_testpypi() {
 }
 
 function python_packaging_upload_pypip() {
+    type -p numactl >/dev/null || pip install twine
     [[ -d dist ]] && rm -r dist
     [[ -d build ]] && rm -r dist
     python -m build . --wheel
@@ -37,7 +44,7 @@ function python_packaging_upload_pypip() {
 function python_pyright_stubs_from_requirements_txt() {
     type -p pyright >/dev/null || pip install pyright
     pip show requirements-parser >/dev/null || pip install requirements-parser
-    local pkgs=$(python -c "import requirements;import os;names=[req.name for req in requirements.parse(open('requirements.txt', 'r'))]; print(' '.join(names))")
+    local pkgs=$(python -c "import requirements; import os; names=[req.name for req in requirements.parse(open('requirements.txt', 'r'))]; print(' '.join(names))")
     for pkg in $pkgs; do
         pyright --createstub $pkg
     done
