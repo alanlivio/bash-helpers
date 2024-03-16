3240b0e4d887a4027977eb01970ea32e4322438c
Alan Guedes <alanlivio@outlook.com>
2024-03-16 15:25:55 +0000

    add exiftool_rm_media_data

--- programs/exiftool.bash
+++ programs/exiftool.bash
@@ -1,5 +1,11 @@
 function exiftool_set_media_all_dates_to_one_year() {
     : ${3?"Usage: ${FUNCNAME[0]} <year>"}
     files=$(find . -iname "*.jpg" -o -iname "*.JPG" -o -iname "*.mp4" -o -iname "*.MP4")
-    exiftool -m -AllDates="$1:01:01 00:00:00"
+    exiftool -m -AllDates="$1:01:01 00:00:00" $files
+}
+
+function exiftool_rm_media_data() {
+    : ${3?"Usage: ${FUNCNAME[0]} <year>"}
+    files=$(find . -iname "*.jpg" -o -iname "*.JPG" -o -iname "*.mp4" -o -iname "*.MP4")
+    exiftool -all= $files
 }
