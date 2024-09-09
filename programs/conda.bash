ef4b5617d9876cda6dfd522f6589da265b13619e
Alan Guedes <alanlivio@outlook.com>
2024-09-09 18:54:31 +0100

    add conda.bash

---
+++ programs/conda.bash
@@ -0,0 +1,18 @@
+function conda_clean() {
+    conda clean --all --y
+}
+
+function conda_env_remove() {
+    : ${1?"Usage: ${FUNCNAME[0]} <env_name>"}
+    conda env remove $1 --y
+}
+
+function conda_env_create_python311() {
+    : ${1?"Usage: ${FUNCNAME[0]} <env_name>"}
+    conda create -n $1 python==3.11 -y
+}
+
+alias conda_env_export_pip_requirements="conda list -e requirements.txt"
+alias conda_env_export_to_enviroment_yml="conda env export -f environment.yml"
+alias conda_env_create_from_enviroment_yml="conda env create -f environment.yml"
+alias conda_env_update_from_enviroment_yml="conda env update -f environment.yml --prune"
--- programs/python.bash
+++ programs/python.bash
@@ -54,8 +54,3 @@ function python_pyright_stubs_from_requirements_txt() {
         pyright --createstub $pkg
     done
 }
-
-alias conda_env_export_pip_requirements="conda list -e requirements.txt"
-alias conda_env_export_to_enviroment_yml="conda env export -f environment.yml"
-alias conda_env_create_from_enviroment_yml="conda env create -f environment.yml"
-alias conda_env_update_from_enviroment_yml="conda env update -f environment.yml --prune"
