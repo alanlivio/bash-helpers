1fe1bf4d09d1db7f1e4869684fc6ac0c577e114d
Alan Guedes <alanlivio@outlook.com>
2024-11-11 17:31:58 +0000

    add latex_clean

---
+++ programs/pdflatex.bash
@@ -0,0 +1,3 @@
+function latex_clean() {
+    rm -rf comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf ./*.aux ./*.fls ./*.fdb_latexmk ./*.log
+}
